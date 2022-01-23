cef0699a59a26de4ec9a3e2989a7e781098bb99f
Valentin Knabel <dev@vknabel.com>
2022-01-23 19:26:53 +0100

    eagerlyRunPreviousTests

--- runtime/prelude-callable-type.go
+++ runtime/prelude-callable-type.go
@@ -1,3 +1,29 @@
 package runtime
 
+import "github.com/vknabel/go-lithia/ast"
+
 var PreludeFunctionTypeRef = MakeRuntimeTypeRef("Function", "prelude")
+
+func Call(function RuntimeValue, args []Evaluatable, fromExpr ast.Expr) (RuntimeValue, *RuntimeError) {
+	callable, ok := function.(CallableRuntimeValue)
+	if !ok {
+		return nil, NewRuntimeErrorf("%s is not callable", function).CascadeCall(nil, fromExpr)
+	}
+
+	arity := callable.Arity()
+	if len(args) < arity {
+		return MakeCurriedCallable(callable, args), nil
+	}
+	intermediate, err := callable.Call(args[:arity], fromExpr)
+	if err != nil {
+		return nil, err.CascadeCall(callable, fromExpr)
+	}
+	if len(args) == arity {
+		return intermediate, nil
+	}
+	if g, ok := intermediate.(CallableRuntimeValue); ok {
+		return g.Call(args[arity:], fromExpr)
+	} else {
+		return nil, NewRuntimeErrorf("%s is not callable", g).CascadeCall(nil, fromExpr)
+	}
+}
